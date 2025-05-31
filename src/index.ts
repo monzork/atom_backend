bba6450aa8dee86a7c6535d10e6c90d108bd3b5e
Marlon Alonzo <marlon.a@phonecheck.com>
2025-05-31 01:32:10 -0600

    fix: removed lib from version control

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './interfaces/routes';
+import { onRequest } from 'firebase-functions/v2/https';
 require('dotenv').config();
 
 const app = express();
@@ -14,4 +15,5 @@ app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
 
+export const api = onRequest(app);
 export default app;
